e579f9ce0c27f5c94946841a05dbc4bab0e89c3e
ZHAO Yao <ytinirt@qq.com>
2016-04-07 22:08:45 +0800

    Change: 用户空间程序周期性打印共享内存中的数据

--- uef_userland.c
+++ uef_userland.c
@@ -9,26 +9,35 @@ int main(int argc, char *argv[])
 {
     int fd;
     void *map;
+    int n;
 
     fd = open("/dev/rx_pool", O_RDWR);
     if (fd < 0) {
         printf("open failed\n");
-        return -1;
+        goto err_out;
     }
 
     map = mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (map == MAP_FAILED) {
         printf("mmap failed\n");
-        goto err_out;
+        goto err_out1;
     }
 
-    printf("%s\n", (char *)map);
+    for (n = 0; n < 10; n++) {
+        printf("[%d] %s\n", n, (char *)map);
+        sleep(5);
+    }
+
     munmap(map, PAGE_SIZE);
+    if (close(fd) != 0) {
+        perror("close");
+    }
 
     return 0;
 
+err_out1:
+    close(fd);
 err_out:
-    munmap(map, PAGE_SIZE);
     return -1;
 }
 
